bba051c5aca042ee4a2df749e7cf9f94d02935d2
afiqarif <akamarulzaidi@gmail.com>
2026-09-11 14:26:02 +0800

    fix jwt generator utils

--- gateway/src/utils/utils.h
+++ gateway/src/utils/utils.h
@@ -2,10 +2,11 @@
 #define UTILS_H
 
 #include <json/json.h>
+#include <jwt-cpp/jwt.h>
+#include <jwt-cpp/traits/open-source-parsers-jsoncpp/traits.h>
 #include <drogon/drogon.h>
 #include <unordered_map>
 #include <vector>
-#include <jwt-cpp/jwt.h>
 #include <chrono>
 
 namespace utils {
@@ -13,15 +14,21 @@ namespace utils {
     using StringMap = std::unordered_map<std::string, std::string>;     // Map for parsed payload
     using FieldList = std::vector<std::string>;                         // Vector for required field
     using DatabaseClient = std::shared_ptr<drogon::orm::DbClient>;      // DbClient data type
+    using jsoncpp_traits = jwt::traits::open_source_parsers_jsoncpp;    // JsonCpp traits
+    using jwt_claim = jwt::basic_claim<jsoncpp_traits>;                       // Claim alias for jwt-cpp using JsonCpp
 
     // Inline functions
+    inline auto jwt_create() {
+        return jwt::create<jsoncpp_traits>();
+    }
+
     inline std::string generateToken(
         const std::string& userId,
         const std::string& role,
         const std::string& secret
     )
     {
-        auto token = jwt::create()
+        return jwt_create()
             .set_issuer("eventflow-gateway")
             .set_type("JWS")
             .set_issued_at(
@@ -35,16 +42,14 @@ namespace utils {
             // Custom payload claims (must wrap native types in jwt::claim)
             .set_payload_claim(
                 "userId",
-                jwt::claim(userId)
+                jwt_claim(Json::Value(userId))
             )
             .set_payload_claim(
                 "role",
-                jwt::claim(role)
+                jwt_claim(Json::Value(role))
             )
             // Sign the token cryptographically using HMAC SHA-256
             .sign(jwt::algorithm::hs256{secret});
-
-        return token;
     }
 
     // Utils functions
